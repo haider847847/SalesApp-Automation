54903103b9ae9e6a02bd7ce434e7d01f9fc6af12
unknown <haider1.editor@gmail.com>
2026-04-20 14:08:12 -0700

    Initial commit

---
+++ Pages/Login.js
@@ -0,0 +1,27 @@
+const { expect } = require("@playwright/test");
+
+class login 
+{
+    constructor (page)
+    {
+        this.page = page
+        this.email = this.page.locator ("(//input[@placeholder='Enter your email address…'])[1]")
+        this.password = this.page.locator ("//input[@type='password']")
+        this.loginclick = this.page.locator ("//button[normalize-space()='Log in now']")
+    } 
+
+    async loginToApplication(email,password)
+    {
+        await this.email.fill(email);
+        await this.password.fill(password)
+        await Promise.all(
+            [
+                this.loginclick.click(),
+                this.page.waitForURL(/.*\/feed\/gilroy/, { timeout: 40000 })
+            ]
+        )
+    }
+}
+
+
+module.exports = login
---
+++ tests/example.spec.js
@@ -0,0 +1,43 @@
+const { test, expect } = require('@playwright/test');
+const login = require ("../Pages/Login")
+const testdata = require ("../testdata.json")
+test.describe.configure({ mode: 'serial' });
+
+
+test ("Login to SalesApp", async function({page})
+{
+  await page.goto("https://xiqonline.com/auth/login/?&redirectURL=https://salesapp.xiqonline.com")
+  const loginClick = new login (page)
+  await loginClick.loginToApplication
+  (
+    testdata.email,
+    testdata.password
+  )
+
+  //Certification pop up cacel
+  await page.getByRole('button', { name: 'Register now' }).nth(2).click()
+
+  // SAVE SESSION AFTER LOGIN
+  await page.context().storageState({ path: 'auth.json' })
+  
+  
+}
+)
+
+
+test.describe('After Login Tests', () => {
+test.use({ storageState: 'auth.json' })
+
+test ("Logout from SalesApp", async function({page})
+{
+  await page.goto("https://salesapp.xiqonline.com")
+  //Logout from bottom left button 
+  await page.getByRole('button', { name: 'Profile picture for menu' }).click();
+  await page.getByRole('button', { name: 'Logout' }).click();
+  await Promise.all([
+   page.getByRole('button', { name: 'Yes' }).click(),
+   expect(page).toHaveURL("https://xiqonline.com/auth/login/?&redirectURL=https://salesapp.xiqonline.com&navigateTo=/feed")
+  ])
+}) 
+
+})
